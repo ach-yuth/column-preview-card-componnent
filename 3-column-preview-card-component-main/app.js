7570004ae3ebce525bfcfd7e1ff6967a1d45528f
Ramakanth Muthyala <ramakanthmuthyala008@gmail.com>
2023-12-23 06:43:20 -0800

    column-preview-card-component

---
+++ 3-column-preview-card-component-main/app.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".box-1 button");
+btn.addEventListener("click", () => {
+  btn.classList.toggle("click-to-change");
+});
+const btn2 = document.querySelector(" .box-2 button");
+btn2.addEventListener("click", () => {
+  btn2.classList.toggle("btn-2");
+});
+const btn3 = document.querySelector(" .box-3 button");
+btn3.addEventListener("click", () => {
+  btn3.classList.toggle("btn-3");
+});
